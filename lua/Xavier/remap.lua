886b3e42ee1d16e44d5172c452fd5193db528230
Xavier Désilets <xavier.desilets@unity3d.com>
2024-02-08 10:09:00 -0500

    Add remap for tabs

--- lua/Xavier/remap.lua
+++ lua/Xavier/remap.lua
@@ -42,6 +42,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- tab remaps
+vim.keymap.set("n", "<leader>nt", "<cmd>tabnew %<CR>");
+vim.keymap.set("n", "<leader>nn", "<cmd>tabnext<CR>");
+
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', {silent = true, noremap = true});
