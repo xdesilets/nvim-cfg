62fdce3b6d7ac049ac393e23e19a6df614ddcee1
Xavier Désilets <xavier.desilets@unity3d.com>
2024-02-09 10:24:23 -0500

    Add F5 continue for dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -16,3 +16,4 @@ end
 vim.keymap.set("n", "<Leader>dt", ':DapToggleBreakpoint<CR>')
 vim.keymap.set("n", "<Leader>dx", ':DapTerminate<CR>')
 vim.keymap.set("n", "<Leader>do", ':DapStepOver<CR>')
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
