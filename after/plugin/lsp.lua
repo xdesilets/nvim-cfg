790134730883803a97e8d344c8a59e7c0e7a75b8
Xavier Désilets <xavier.desilets@unity3d.com>
2024-02-13 09:30:18 -0500

    Changing go to declaration keys

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -25,7 +25,7 @@ lsp_zero.set_preferences({
 lsp_zero.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "ge", function() vim.lsp.buf.declaration() end, opts)
     vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
